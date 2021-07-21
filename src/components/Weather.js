18cdd2404bcd997c7661993ed4073ad0e3c36443
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-07-21 15:29:51 +0100

    Initial commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,31 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import '../styling/weather.scss';
+import Search from './Search';
 
+const { search }  = window.location;
+let city = new URLSearchParams(search).get('city')
+console.log(city)
 const Weather = () => {
-  const [weather, updateWeather] = useState(null)
+  const [weather, updateWeather] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => {
-      updateWeather(res.data.weather[0].description)
+    axios.get(getApiCall(city)).then(res => {
+      for(let i = 0; i < 5; i++){
+        updateWeather(prevArray => [...prevArray, (res.data.list[i].weather[0].description)])
+      }
     })
   }, [])
 
+  var getApiCall = cityName => `http://api.openweathermap.org/data/2.5/forecast/?q=${cityName}&cnt=5&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`;
+  var count = 1
+  const weatherForecast = weather.map((day) => <li key = {count++}>{day}</li>);
+
   return (
     <div data-testid="weather" className="weather">
+    <Search />
       Weather:
-      <br />
-      The current weather in London is: {weather}
+      The 5 day forcast of {city} is: <ul> {weatherForecast} </ul>
     </div>
   );
 }
